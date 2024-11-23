660df32374c84efd0684be4f9e9dad083f2b2385
dorrin-sot <59933477+dorrin-sot@users.noreply.github.com>
2024-11-23 20:06:26 +0000

    implementation for division #17

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -14,7 +14,7 @@ public class Calculator {
     }
 
     public double divide(double num1, double num2) {
-        return 0.0;
+        return num1 / num2;
     }
 
     public double sqrt(double num) {
