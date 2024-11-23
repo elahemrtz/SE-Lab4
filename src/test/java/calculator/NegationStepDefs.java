25782d9377334667e14d5925859be77bd8c13148
ElaheMrtz <62252751+elahemrtz@users.noreply.github.com>
2024-11-23 19:12:30 +0000

    stepdef for negation #13

--- src/test/java/calculator/NegationStepDefs.java
+++ src/test/java/calculator/NegationStepDefs.java
@@ -1,5 +1,28 @@
 package calculator;
 
+import static org.junit.Assert.assertEquals;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class NegationStepDefs {
-    
+    private Calculator calculator = new Calculator();
+
+    private double num, result;
+
+    @Given("a number, {double}")
+    public void a_number(double num) {
+        this.num = num;
+    }
+
+    @When("it is negated")
+    public void it_is_negated() {
+        this.result = calculator.negate(this.num);
+    }
+
+    @Then("the negation result is {double}")
+    public void the_negation_result_is(double expectedResult) {
+        assertEquals(expectedResult, this.result, 0);
+    }
 }
