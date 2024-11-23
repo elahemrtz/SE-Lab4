d89d802fa54fd0b4b6a494a47a78cc2e943c72a4
dorrin-sot <59933477+dorrin-sot@users.noreply.github.com>
2024-11-23 19:59:31 +0000

    implementation for add #14

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,7 +2,7 @@ package calculator;
 
 public class Calculator {
     public double add(double num1, double num2) {
-        return 0.0;
+        return num1 + num2;
     }
 
     public double minus(double num1, double num2) {
