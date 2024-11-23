8c9c7d1cf43856594fa1a38ef239e73240ef526b
ElaheMrtz <62252751+elahemrtz@users.noreply.github.com>
2024-11-23 19:02:01 +0000

    implementation for multiplication #16

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -10,7 +10,7 @@ public class Calculator {
     }
 
     public double multiply(double num1, double num2) {
-        return 0.0;
+        return num1 * num2;
     }
 
     public double divide(double num1, double num2) {
