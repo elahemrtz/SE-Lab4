1b5574f5cf03aa3bfd2143e377feb91a3682b5c3
fatemeh-fo <63150708+fatemeh-fo@users.noreply.github.com>
2024-11-23 19:39:04 +0000

    stepdef for sqrt #12

--- src/test/java/calculator/SqrtStepDefs.java
+++ src/test/java/calculator/SqrtStepDefs.java
@@ -1,5 +1,28 @@
 package calculator;
 
+import static org.junit.Assert.assertEquals;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class SqrtStepDefs {
-    
+    private Calculator calculator = new Calculator();
+
+    private double num, result;
+
+    @Given("a number for sqrt, {double}")
+    public void a_number_for_sqrt(double num) {
+        this.num = num;
+    }
+
+    @When("it is sqrt")
+    public void it_is_sqrt() {
+        this.result = calculator.sqrt(this.num);
+    }
+
+    @Then("the sqrt result is {double}")
+    public void the_sqrt_result_is(double expectedResult) {
+        assertEquals(expectedResult, this.result, 0.00000001);
+    }
 }
